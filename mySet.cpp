3a08460b82093d41db364300a58f813603ea26fa
dhunganab2 <dhunganabijay85@gmail.com>
2025-03-03 15:23:30 -0500

    Initial Commit

---
+++ mySet.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Bijay Dhungana on 3/2/25.
+//
+
+#include "mySet.h"
+using namespace std;
---
+++ mySet.h
@@ -0,0 +1,20 @@
+//
+// Created by Bijay Dhungana on 3/2/25.
+//
+
+#ifndef MYSET_H
+#define MYSET_H
+#include <vector>
+
+
+
+class mySet {
+protected:
+    vector <int> elts;
+    int size;
+
+};
+
+
+
+#endif //MYSET_H
